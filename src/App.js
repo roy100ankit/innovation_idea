e971cccbd75af027575d36ea478146a27430b1bd
Ankit Roy <ankitroy@amdocs.com>
2022-04-27 14:06:15 +0530

    Displaying card templates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import MainPage from './components/mainPage'
 
 function App() {
   return (
     <div className="App">
-      Main Project
+      {/* <Switch>  
+       <Route exact path='/' component={MainPage}/>
+      </Switch> */}
+      <MainPage />
     </div>
   );
 }
